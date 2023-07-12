7d5cc96ab9f6229c0577e5836caa6a587aa040ec
Samuell <samuelltorres145@gmail.com>
2023-07-12 17:25:03 -0300

    added offers carousel in home-page

---
+++ src/components/OffersCarousel/index.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react'
+import Carousel from 'react-elastic-carousel'
+
+import Offers from '../../assets/offers.png'
+import api from '../../services/api'
+import { Container, CategoryImg, ConteinerItems, Image, Button } from './styles'
+
+function OffersCarousel() {
+  const [offers, setOffers] = useState([])
+
+  useEffect(() => {
+    async function loadOffers() {
+      const { data } = await api.get('products')
+
+      const onlyOffers = data.filter(product => product.offer)
+
+      setOffers(onlyOffers)
+    }
+
+    loadOffers()
+  }, [])
+
+  const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 400, itemsToShow: 2 },
+    { width: 600, itemsToShow: 3 },
+    { width: 900, itemsToShow: 4 },
+    { width: 1300, itemsToShow: 5 },
+  ]
+
+  return (
+    <Container>
+      <CategoryImg src={Offers} alt="logo da oferta" />
+
+      <Carousel
+        itemsToShow={5}
+        style={{ width: '90%' }}
+        breakPoints={breakPoints}
+      >
+        {offers &&
+          offers.map(product => (
+            <ConteinerItems key={product.id}>
+              <Image src={product.url} alt="foto da oferta" />
+              <p>{product.name}</p>
+              <p>R$ {product.price}</p>
+
+              <Button>Peça agora</Button>
+            </ConteinerItems>
+          ))}
+      </Carousel>
+    </Container>
+  )
+}
+
+export default OffersCarousel
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import HomeLogo from '../../assets/home-logo.svg'
 import CategoryCarousel from '../../components/CategoryCarousel'
+import OffersCarousel from '../../components/OffersCarousel'
 import { Container, HomeImg } from './styles'
 function Home() {
   return (
     <Container>
       <HomeImg src={HomeLogo} alt="logo da home" />
       <CategoryCarousel />
+      <OffersCarousel />
     </Container>
   )
 }
