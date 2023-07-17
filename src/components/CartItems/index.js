659b822b4595976a2dff9f1fbc5e94b57e870e9e
Samuell <samuelltorres145@gmail.com>
2023-07-17 19:20:51 -0300

    added styling to buttons

--- src/components/CartItems/index.js
+++ src/components/CartItems/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { useCart } from '../../hooks/CartContext'
 import formatCurrency from '../../utils/formatCurrency'
-import { Container, Header, Body } from './styles'
+import { Container, Header, Body, EmptyCart } from './styles'
 
 export function CartItems() {
   const { cartProducts } = useCart()
@@ -15,20 +15,29 @@ export function CartItems() {
         <p></p>
         <p>Itens</p>
         <p>Preço</p>
-        <p>Quantidade</p>
+        <p style={{ paddingRight: 30 }}>Quantidade</p>
         <p>Total</p>
       </Header>
 
-      {cartProducts &&
+      {cartProducts && cartProducts.length > 0 ? (
         cartProducts.map(product => (
           <Body key={product.id}>
             <img src={product.url} />
             <p>{product.name}</p>
-            <p>{formatCurrency(product.price)}</p>
-            <p>{product.quantity}</p>
-            <p>{formatCurrency(product.quantity * product.price)}</p>
+            <p className="price">{formatCurrency(product.price)}</p>
+            <div className="quantity-container">
+              <button>-</button>
+              <p>{product.quantity}</p>
+              <button>+</button>
+            </div>
+            <p className="price-total">
+              {formatCurrency(product.quantity * product.price)}
+            </p>
           </Body>
-        ))}
+        ))
+      ) : (
+        <EmptyCart> Carrinho vazio</EmptyCart>
+      )}
     </Container>
   )
 }
--- src/components/CartItems/styles.js
+++ src/components/CartItems/styles.js
@@ -1,7 +1,68 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  background-color: #fff;
+  box-shadow: 0px 10px 40px rgba(0, 0, 0, 0.03);
+  border-radius: 30px;
+  padding: 10px;
+  width: max-content;
+`
 
-export const Header = styled.div``
+export const Header = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  padding: 10px;
+  border-bottom: 1px solid #b5b5b5;
 
-export const Body = styled.div``
+  p {
+    color: #9a9a9d;
+    font-size: 17px;
+  }
+`
+
+export const Body = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  width: max-content;
+  grid-gap: 10px 15px;
+  padding: 10px;
+
+  img {
+    border-radius: 20px;
+    width: 135px;
+  }
+
+  p {
+    font-size: 16px;
+    color: #000;
+  }
+
+  .price {
+    font-weight: bold;
+  }
+
+  .price-total {
+    font-weight: bold;
+  }
+
+  .quantity-container {
+    display: flex;
+    gap: 20px;
+    align-items: baseline;
+    font-weight: bold;
+
+    button {
+      height: 30px;
+      background: transparent;
+      border: none;
+      font-size: 24px;
+      cursor: pointer;
+    }
+  }
+`
+
+export const EmptyCart = styled.p`
+  padding: 20px;
+  text-align: center;
+  font-weight: bold;
+`
