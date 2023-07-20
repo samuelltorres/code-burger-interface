7dd3ec2a0a7f87a4999d930afeaaf828cca9fc22
Samuell <samuelltorres145@gmail.com>
2023-07-20 20:46:42 -0300

    finished order table

--- src/containers/Admin/Orders/index.js
+++ src/containers/Admin/Orders/index.js
@@ -1,10 +1,5 @@
 import React, { useEffect, useState } from 'react'
 
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
-import Box from '@mui/material/Box'
-import Collapse from '@mui/material/Collapse'
-import IconButton from '@mui/material/IconButton'
 import Paper from '@mui/material/Paper'
 import Table from '@mui/material/Table'
 import TableBody from '@mui/material/TableBody'
@@ -12,23 +7,25 @@ import TableCell from '@mui/material/TableCell'
 import TableContainer from '@mui/material/TableContainer'
 import TableHead from '@mui/material/TableHead'
 import TableRow from '@mui/material/TableRow'
-import Typography from '@mui/material/Typography'
-import PropTypes from 'prop-types'
 
 import api from '../../../services/api'
-import { Container } from './styles'
+import formatDate from '../../../utils/formatDate'
+import status from './order-status'
+import Row from './row'
+import { Container, Menu, LinkMenu } from './styles'
 
 export function Orders() {
   const [orders, setOrders] = useState([])
+  const [filteredOrders, setFilteredOrders] = useState([])
+  const [activeStatus, setActiveStatus] = useState(1)
   const [rows, setRows] = useState([])
-
-  // console.log(orders)
 
   useEffect(() => {
     async function loadOrders() {
       const { data } = await api.get('orders')
 
       setOrders(data)
+      setFilteredOrders(data)
     }
 
     loadOrders()
@@ -38,22 +35,77 @@ export function Orders() {
     return {
       name: order.user.name,
       orderId: order._id,
-      data: order.createdAt,
+      date: formatDate(order.createdAt),
       status: order.status,
       products: order.products
     }
   }
 
   useEffect(() => {
-    const newRows = orders.map(ord => createData(ord))
+    const newRows = filteredOrders.map(ord => createData(ord))
     setRows(newRows)
+  }, [filteredOrders])
+
+  useEffect(() => {
+    if (activeStatus === 1) {
+      setFilteredOrders(orders)
+    } else {
+      const statusIndex = status.findIndex(sts => sts.id === activeStatus)
+      const newFilteredOrders = orders.filter(
+        order => order.status === status[statusIndex].value
+      )
+      setFilteredOrders(newFilteredOrders)
+    }
   }, [orders])
 
-  console.log(rows)
+  function handleStatus(status) {
+    if (status.id === 1) {
+      setFilteredOrders(orders)
+    } else {
+      const newOrders = orders.filter(order => order.status === status.value)
+      setFilteredOrders(newOrders)
+    }
+    setActiveStatus(status.id)
+  }
 
   return (
     <Container>
-      <h1>Pedidos</h1>
+      <Menu>
+        {status &&
+          status.map(status => (
+            <LinkMenu
+              key={status.id}
+              onClick={() => handleStatus(status)}
+              isActiveStatus={activeStatus === status.id}
+            >
+              {status.label}
+            </LinkMenu>
+          ))}
+      </Menu>
+
+      <TableContainer component={Paper}>
+        <Table aria-label="collapsible table">
+          <TableHead>
+            <TableRow>
+              <TableCell />
+              <TableCell>Pedido</TableCell>
+              <TableCell>Cliente</TableCell>
+              <TableCell>Data do pedido</TableCell>
+              <TableCell>Status</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map(row => (
+              <Row
+                key={row.orderId}
+                row={row}
+                setOrders={setOrders}
+                orders={orders}
+              />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Container>
   )
 }
--- src/containers/Admin/Orders/styles.js
+++ src/containers/Admin/Orders/styles.js
@@ -1,6 +1,37 @@
+import ReactSelect from 'react-select'
+
 import styled from 'styled-components'
 
 export const Container = styled.div`
   background-color: #efefef;
   min-height: 100vh;
+  padding: 20px;
+`
+
+export const ProductsImg = styled.img`
+  width: 60px;
+  border-radius: 5px;
+`
+
+export const ReactSelectStyle = styled(ReactSelect)`
+  width: 250px;
+
+  .css-13cymwt-control {
+    cursor: pointer;
+  }
+`
+
+export const Menu = styled.div`
+  display: flex;
+  gap: 50px;
+  justify-content: center;
+  margin: 20px 0;
+`
+
+export const LinkMenu = styled.a`
+  cursor: pointer;
+  color: #323d5d;
+  padding: 13px;
+  font-weight: ${props => (props.isActiveStatus ? 'bold' : 'normal')};
+  border-bottom: ${props => props.isActiveStatus && '2px solid #9758A6'};
 `
