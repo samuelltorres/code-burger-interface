0e97b6754f49de104ea530fb6f1af226e581a642
Samuell <samuelltorres145@gmail.com>
2023-07-24 14:26:05 -0300

    styling new product form

--- src/containers/Admin/NewProduct/styles.js
+++ src/containers/Admin/NewProduct/styles.js
@@ -1,7 +1,45 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+import { Button } from '../../../components'
 
-export const Label = styled.p``
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-export const Input = styled.input``
+  form {
+    border-radius: 10px;
+    background: #565656;
+    padding: 30px;
+  }
+`
+
+export const Label = styled.p`
+  font-size: 14px;
+  color: #fff;
+  margin-bottom: 3px;
+`
+
+export const Input = styled.input`
+  width: 100%;
+  height: 50px;
+
+  background: #fff;
+  border: none;
+  border-radius: 8px;
+  box-shadow: 0px 4px 14px 0px rgba(0, 0, 0, 0.1);
+
+  margin-bottom: 22px;
+
+  /* color: #333;
+  font-size: 16px;
+  font-weight: 400; */
+`
+
+export const ButtonStyles = styled(Button)`
+  width: 100%;
+  height: 48px;
+  margin-top: 120px;
+`
--- src/containers/Admin/styles.js
+++ src/containers/Admin/styles.js
@@ -4,10 +4,11 @@ export const Container = styled.div`
   display: flex;
   justify-content: flex-start;
 
-  /* min-height: 100%; */
-  /* width: 100vw; */
+  min-height: 100vh;
+  width: 100vw;
   background-color: #efefef;
 `
 export const ContainerItems = styled.div`
   padding: 20px;
+  width: 100%;
 `
